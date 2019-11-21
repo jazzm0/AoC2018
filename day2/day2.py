c0a3bc66675ca3d378b57502f0196a5f01a732bd
Tibor Tarnai <tibor.tarnai@sap.com>
2019-11-21 10:50:01 +0100

    result part#2 Day2

--- day2/day2.py
+++ day2/day2.py
@@ -3,18 +3,37 @@ from collections import defaultdict
 x = 0
 y = 0
 
+str_list = []
+
+
+def one_apart(a, b):
+    length = min(len(a), len(b))
+    k, diff, result = 0, 0, ''
+    while k < length:
+        if a[k] != b[k]:
+            diff += 1
+        else:
+            result = result + a[k]
+        k += 1
+        if diff > 1:
+            return
+
+    print(result)
+
+
 with open('input') as ifile:
     for line in ifile:
-        print(line)
         d = defaultdict(int)
         for i in line[0:-1]:
             d[i] = d[i] + 1
-        print(d)
 
         if 2 in d.values():
             x += 1
         if 3 in d.values():
             y += 1
+        for j in str_list:
+            one_apart(j, line)
+        str_list.append(line)
 
 print('x = {}, y = {}'.format(x, y))
 
