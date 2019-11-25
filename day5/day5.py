9943095318d7a1f4b2dc7def713ebe4b6c55b638
Tibor Tarnai <tibor.tarnai@sap.com>
2019-11-25 17:58:18 +0100

    solution day5 part#1

---
+++ day5/day5.py
@@ -0,0 +1,27 @@
+import re
+
+
+def do_react(a, b):
+    return a.upper() == b.upper() and a != b
+
+
+def remove_reactive(a, replace):
+    pattern = re.compile(a, re.IGNORECASE)
+    return pattern.sub("", replace)
+
+
+def reduce(to_be_reduced):
+    i = 0
+    while i < len(to_be_reduced) - 1:
+        if do_react(to_be_reduced[i], to_be_reduced[i + 1]):
+            to_be_reduced = to_be_reduced.replace(to_be_reduced[i] + to_be_reduced[i + 1], '')
+            if i > 1:
+                i -= 1
+        else:
+            i += 1
+    return to_be_reduced
+
+
+with open('input') as ifile:
+    for line in ifile:
+        print(len(reduce(line[:-1])))
