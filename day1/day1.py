55cbc2a090c9c682b4bcd3f49f05441986de4a34
Tibor Tarnai <tibor.tarnai@sap.com>
2019-11-28 14:31:14 +0100

    Day1 part#2

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,22 @@
 s = 0
 
+numbers = []
+sums = set()
+
 with open('input') as ifile:
     for line in ifile:
-        s += int(line)
+        n = int(line)
+        numbers.append(n)
+        s += n
+        sums.add(s)
 
 print(s)
+
+while True:
+    for i in numbers:
+        s += i
+        if s in sums:
+            print(s)
+            exit()
+        else:
+            sums.add(s)
